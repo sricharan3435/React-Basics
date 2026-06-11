d236d56ff1f768d4d671af2e7ab486f99f155ade
sricharan3435 <sricharan3435@gmail.com>
2026-06-11 12:47:55 +0530

    Button with Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import Card from './Card.jsx'
+import Button from './Button.jsx'
 
 function App(){
-  return(
-    <>
-      <Card />
-      <Card />
-      <Card />
-    </>
-  );
+  return(<Button />);
 }
 
 export default App
